e0c9cd529d487bfde1e22bee13c9482111ff1b8b
Aidsan58 <aidanr1@comcast.net>
2025-12-17 13:59:31 -0800

    Created Canvas

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import * as THREE from 'three';
+import { Canvas } from '@react-three/fiber'
 import './index.css'
 import App from './App.jsx'
+import * as THREE from 'three';
+
+function App() {
+  return (
+    <div id="canvas-container">
+      <Canvas>
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
