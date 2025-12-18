f330a8dfd13e2709f13420bdb920918d6515e0e7
Aidsan58 <aidanr1@comcast.net>
2025-12-17 17:35:37 -0800

    Revert "Added arguments to App"
    This reverts commit 60cde87b7b737dad021f4275e5c1f09776bf513e.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import TestScene from './components/TestScene';
 
 function App() {
-  return <TestScene />;
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      </div>
+      {/* <h1>WebGL-Molecule-Viewer</h1>
+      <LoadPDBFile>Load PDB File</LoadPDBFile> */}
+      <PDBScene />
+    </>
+  )
 }
 
 export default App;
