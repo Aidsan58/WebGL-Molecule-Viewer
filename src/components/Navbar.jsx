4eb5b533a0c0643818e0638ea10e4369cca4c5a7
Aidsan58 <aidanr1@comcast.net>
2025-12-20 23:31:23 -0800

    Fixed Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,13 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { Link } from "react-router-dom";
 
-function Layout() {
+function Navbar() {
   return (
-    <>
-      <Navbar />
-      <main>
-        <Outlet />
-      </main>
-    </>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/dashboard">Dashboard</Link>
+    </nav>
   );
 }
 
-export default Layout;
+export default Navbar;
