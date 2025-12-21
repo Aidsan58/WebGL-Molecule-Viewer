d575d734b7ea330b3ad40b8c3147360c86edd5b3
Aidsan58 <aidanr1@comcast.net>
2025-12-20 23:29:48 -0800

    Removed Navbar from Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,5 @@
 import { Link } from "react-router-dom";
 
-function Navbar() {
-  return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/dashboard">Dashboard</Link>
-    </nav>
-  );
-}
-
-
 function Home() {
     return (
         <>
