cf75e16e197c03436fb4571b2813cb2dc20b524a
Aidsan58 <aidanr1@comcast.net>
2025-12-21 00:26:12 -0800

    Fixed pathing in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <nav>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
-      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/molecule-viewer">Game</Link>
     </nav>
   );
 }
