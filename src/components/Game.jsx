ce4bdc030218c23565da94eafa9e9f3106e486d0
Aidsan58 <aidanr1@comcast.net>
2025-12-20 22:53:27 -0800

    Added functionality to Game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,17 @@
 function Game() {
-    <h1>Game</h1>
+    return (
+        <>
+    {/* UI LAYER */}
+      <h1>WebGL Molecule Viewer</h1>
+      <div className="ui">
+        <LoadPDBFile onLoad={setAtoms} />
+      </div>
+
+      {/* 3D SCENE */}
+      <PDBScene atoms={atoms} />
+      </>
+    )
+    
 }
 
 export default Game;
