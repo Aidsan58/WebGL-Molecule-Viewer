4994483aa401c29390ff92dd7bdd937e91fcf464
Aidsan58 <aidanr1@comcast.net>
2025-12-20 23:13:07 -0800

    Added Navbar function

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/dashboard">Dashboard</Link>
+    </nav>
+  );
+}
+
+
 function Home() {
-    return <h1>Front Page</h1>;
+    return (
+        <>
+            <h1>Front Page</h1>
+            <Navbar />
+        </>
+    )
 }
 
 export default Home;
