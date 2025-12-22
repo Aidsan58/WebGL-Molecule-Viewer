e6a6c8cb3724a9f123398fc46fd4562613342b1d
Aidsan58 <aidanr1@comcast.net>
2025-12-21 16:36:41 -0800

    Added logo to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
-import './../Navbar.css'
+import "./../Navbar.css";
+import logo from "../assets/images.png";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/molecule-viewer">Molecule Renderer</Link>
-    </nav>
+    <header className="header">
+      <a href="/" className="logo"> <img src={logo} alt="Logo" /> </a>
+
+      <nav className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/molecule-viewer">Molecule Renderer</Link>
+      </nav>
+    </header>
   );
 }
 
