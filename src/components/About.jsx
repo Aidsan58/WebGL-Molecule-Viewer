1b20e5bceb31aacc89a564601efdc7c2b93065c5
Aidsan58 <aidanr1@comcast.net>
2025-12-21 18:17:57 -0800

    Cleaned up About. Renamed home-header to about-header to prevent clashes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,21 +2,28 @@ import "./../About.css";
 
 function About() {
   return (
-    <>
-        <div className="home-header">
-            <h1><span id="ABOUT">ABOUT</span></h1>
-            <p>This website takes protein data bank (.PDB) files and renders a scale-accurate model of that protein.</p>
-            <p>Technologies used:</p>
-            <ul>
-              <li>React as library framework</li>
-              <li>WebGL and Three.js for graphics</li>
-              <li>React Three Fiber for integration between React with JSX and Three.js</li>
-              <li>React-Router-DOM for client-side routing</li>
-              <li>Colors are based on CPK values https://en.wikipedia.org/wiki/CPK_coloring</li>
-              <li>Logo source: https://www.flaticon.com/free-icons/chemistry</li>
-            </ul>
-        </div>
-    </>
+    <div className="about-header">
+      <h1><span id="ABOUT">ABOUT</span></h1>
+
+      <ul className="about-list">
+        <li>React as library framework</li>
+        <li>WebGL and Three.js for graphics</li>
+        <li>React Three Fiber for integration between React with JSX and Three.js</li>
+        <li>React-Router-DOM for client-side routing</li>
+        <li>
+          Colors are based on CPK values
+          <a href="https://en.wikipedia.org/wiki/CPK_coloring" target="_blank" rel="noreferrer">
+            (source)
+          </a>
+        </li>
+        <li>
+          Logo source:
+          <a href="https://www.flaticon.com/free-icons/chemistry" target="_blank" rel="noreferrer">
+            Flaticon
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
