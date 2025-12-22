247cdd4cc4f8e972faf3a2899af6bd4758f2ef0d
Aidsan58 <aidanr1@comcast.net>
2025-12-21 17:55:37 -0800

    import home.css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,9 @@
+import "./../Home.css";
+
 function Home() {
     return (
         <>
-            <div class="main-heading">
+            <div className="home-header">
                 <h1><span id="WELCOME">WELCOME</span></h1>
                 <p>This website takes protein data bank (.PDB) files and renders a scale-accurate model of that protein.</p>
             </div>
