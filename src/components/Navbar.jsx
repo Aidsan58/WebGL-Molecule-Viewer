3bb85863f78411025924401da83d2c6b6add52cc
Aidsan58 <aidanr1@comcast.net>
2025-12-21 17:09:52 -0800

    Added image specs to navbar.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,15 @@ import logo from "../assets/images.png";
 function Navbar() {
   return (
     <header className="header">
-      <a href="/" className="logo"> <img src={logo} alt="Logo" /> </a>
+      
 
       <nav className="navbar">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/molecule-viewer">Molecule Renderer</Link>
+        <a href="/" className="logo"> <img src={logo} alt="Logo" /> </a>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/molecule-viewer">Molecule Renderer</Link>
+        </div>
       </nav>
     </header>
   );
